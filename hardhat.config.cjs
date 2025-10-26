9445b848d32c6eaa3babc0c2da1a0aa76f4cb167
Pranav <pranavpipariya@gmail.com>
2025-10-27 01:01:46 +0530

    bruh ab mai kya hi bolun bhai sahi m

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,20 +1,3 @@
-// import { HardhatUserConfig } from "hardhat/config";
-// import "@nomicfoundation/hardhat-ethers";
-// import * as dotenv from "dotenv";
-// dotenv.config();
-
-// const config: HardhatUserConfig = {
-//   solidity: "0.8.20",
-//   networks: {
-//     baseSepolia: {
-//       url: process.env.BASE_SEPOLIA_RPC_URL || "https://sepolia.base.org",
-//       chainId: 84532,
-//       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
-//     },
-//   },
-// };
-// export default config;
-
 // hardhat.config.cjs
 require("dotenv").config();
 require("@nomicfoundation/hardhat-ethers");
